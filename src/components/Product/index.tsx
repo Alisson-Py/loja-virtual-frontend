edc7b7819467fdb8f41959d746d3444c6c80c29e
Alisson-Py <alissilva1231@hotmail.com>
2021-05-31 19:10:18 -0300

    feat: create Product view component

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CurrencyConverter from '../../utils/CurrencyConverter';
+
+import './index.css';
+
+interface ProductsProps {
+  props: {
+    image: string;
+    name: string;
+    description: string;
+    value: number;
+  }
+};
+
+const ProductsView: React.FC<ProductsProps> = ({props}) => {
+  return (
+    <div className="product">
+      <img src={props.image} alt={props.name} />
+      <h3 className="title">{props.name}</h3>
+      <p className="description">{props.description}</p>
+      <p className="value"><strong>{CurrencyConverter(props.value)}</strong><br/>
+        ou
+        <br/> 10x <strong>{CurrencyConverter(Number((props.value / 10).toPrecision()))}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default ProductsView;
