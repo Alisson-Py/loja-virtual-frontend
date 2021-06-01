5e0ddd611ec3ed0ea9f9af4c87fc39e9102b1bac
Alisson-Py <alissilva1231@hotmail.com>
2021-06-01 09:29:35 -0300

    feat: add favorite icon and static image

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CurrencyConverter from '../../utils/CurrencyConverter';
 
 import './index.css';
+import productIcon from '../../assets/product-icon.png';
+import favoriteIcon from '../../assets/favorite-icon.svg';
 
 interface ProductsProps {
   props: {
@@ -15,13 +17,14 @@ interface ProductsProps {
 const ProductsView: React.FC<ProductsProps> = ({props}) => {
   return (
     <div className="product-view">
-      <img src={props.image} alt={props.name} />
+      <img src={productIcon} alt={props.name} className="img" />
       <h3 className="title">{props.name}</h3>
       <p className="description">{props.description}</p>
       <p className="value"><strong>{CurrencyConverter(props.value)}</strong><br/>
         ou
         <br/> 10x <strong>{CurrencyConverter(Number((props.value / 10).toPrecision()))}</strong>
       </p>
+      <img src={favoriteIcon} alt="favorites" className="favorite-button"/>
     </div>
   );
 };
