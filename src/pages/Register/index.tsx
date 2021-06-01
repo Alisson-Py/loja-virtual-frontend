9513ded8fd807e5c3c4224f37eacd209c43a83cc
Alisson-Py <alissilva1231@hotmail.com>
2021-06-01 12:03:06 -0300

    feat: create Register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 
+import './index.css';
+import userIcon from '../../assets/user-icon.svg';
+
 export default function Register() {
+  const [visiblePassword, setVisiblePassord]  = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  function handleRegister() {
+    setLoading(true);
+    setLoading(false);
+
+  }
   return (
     <div className="register">
       <Header title="Register"/>
+      <main>
+        <div/>
+
+        <div className="form">
+          <img src={userIcon} alt="user" className="user-icon" />
+          <div className="input-and-button">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              placeholder="Digite seu nome..."
+              autoComplete='off'
+            />
+
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              placeholder="Digite seu sobrenome..."
+              autoComplete='off'
+            />
+
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Digite seu email..."
+              autoComplete='off'
+            />
+            
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Digite uma senha..."
+              autoComplete='off'
+            />
+            <input
+              type="password"
+              name="repeat-password"
+              id="repeat-password"
+              placeholder="Repita sua senha..."
+              autoComplete='off'
+            />
+
+            <button onClick={handleRegister}>
+              {
+                !loading?
+                <p>Register</p>:
+                <div className="loading-view"/>
+              }
+            </button>
+          </div>
+        </div>
+
+        <div/>
+      </main>
     </div>
   );
 };
