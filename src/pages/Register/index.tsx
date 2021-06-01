20b2973910d3414059f274e6c537873f3d33ccbb
Alisson-Py <alissilva1231@hotmail.com>
2021-06-01 11:49:48 -0300

    feat: create Register page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+export default function Register() {
+  return (
+    <div className="register">
+      <Header title="Register"/>
+    </div>
+  );
+};
