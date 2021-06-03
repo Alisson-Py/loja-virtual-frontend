eacc1ce7fffe25d3299b7fac415898ee7b71c8c2
Alisson-Py <alissilva1231@hotmail.com>
2021-06-02 23:35:29 -0300

    feat: add content page

--- src/pages/ProductsDetails/index.tsx
+++ src/pages/ProductsDetails/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
+
+import './index.css';
+import leftArrowIcon from '../../assets/arrow-left-icon.svg';
+import favoriteIcon from '../../assets/favorite-icon.svg';
+import productIcon from '../../assets/product-icon.png';
 
 export default function ProductsDetails() {
   return (
     <div className="products-details">
       <Header title="Detalhes"/>
+      <main>
+        <div className="header">
+          <Link to="/">
+            <img src={leftArrowIcon} alt="back" />
+          </Link>
+          <h2>title</h2>
+          <button>
+            <img src={favoriteIcon} alt="gostei" />
+          </button>
+        </div>
+        <div className="content">
+          <div className="image">
+            <img src={productIcon} alt="producto" />
+          </div>
+          <div className="content">
+            <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos quam ullam distinctio harum repellendus? Voluptatum soluta maxime facilis reiciendis, nulla, inventore eum.</p>
+            <div className="price-and-shop">
+            <p className="value">11111</p>
+            <button>Comprar</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
