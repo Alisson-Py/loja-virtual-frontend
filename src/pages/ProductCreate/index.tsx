7e5f9357cc2f2a3aea7821388f940d59025a43f1
Alisson-Py <alissilva1231@hotmail.com>
2021-06-04 23:07:29 -0300

    feat: create Product Create page

---
+++ src/pages/ProductCreate/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+export default function ProductCreate() {
+  return (
+    <div className="product-create">
+      <Header title="Criar"/>
+    </div>
+  );
+};
