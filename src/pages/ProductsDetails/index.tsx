2c61a53e378ed16ea36491b2793c8e7d20ed30bd
Alisson-Py <alissilva1231@hotmail.com>
2021-06-07 05:43:23 -0300

    fix: pass props variable in useEffect

--- src/pages/ProductsDetails/index.tsx
+++ src/pages/ProductsDetails/index.tsx
@@ -29,7 +29,7 @@ export default function ProductsDetails(props: RouteComponentProps) {
       console.log(err.message);
     }).finally(() => {
     });
-  },[])
+  },[props])
 
   function handleGoBack() {
     history.goBack()
