e53a849e9e1e1403dd84ef9a801fcc6de10bb584
Alisson-Py <alissilva1231@hotmail.com>
2021-06-09 03:04:53 -0300

    feat: create add credit card page

---
+++ src/pages/AddCart/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header';
+
+import './index.css';
+
+export default function AddCart() {
+  const [fullName, setFullName] = useState<string>();
+  const [cardNumber, sertCardNumber] = useState<number>();
+  const [cardNumberView, sertCardNumberView] = useState<number>();
+  const [expirationDate, setExpirationDate] = useState<string>();
+  const [ccv, setCcv] = useState<number>();
+
+  useEffect(() => {},[]);
+
+  function handleAddCard() {
+    
+  }
+
+  return (
+    <div className="add-card">
+      <Header title="Adicionar Cartao"/>
+      <main>
+        <div className="content">
+          <div className="card-view">
+            <p className="card-number">{'1234 1234 1234 1234'}</p>
+            <div className="botton-card-data">
+              <p className="name">{'Alisson SIlva dos Santos'}</p>
+              <div className="date-and-ccv">
+                <p className="expiration-date">{'12/12'}</p>
+                <p className="ccv">{'***'}</p>
+              </div>
+            </div>
+          </div>
+          <div className="form">
+            <input
+              type="text"
+              name="card-number"
+              id="card-number"
+              placeholder="Digite o nome do titular..."
+              autoComplete='off'
+              value={fullName}
+              onChange={e => setFullName(e.target.value)}
+            />
+            <input
+              type="text"
+              name="card-number"
+              id="card-number"
+              placeholder="Digite o numero do cartao..."
+              autoComplete='off'
+              value={cardNumber}
+              onChange={e => sertCardNumber(Number(e.target.value))}
+            />
+            <div className="date-and-ccv-input">
+              <input
+                type="text"
+                name="expiration-date"
+                id="expiration-date"
+                placeholder="Digite o expiration date..."
+                autoComplete='off'
+                value={expirationDate}
+                onChange={e => setExpirationDate(e.target.value)}
+              />
+              <input
+                type="text"
+                name="ccv"
+                id="ccv"
+                placeholder="Digite o ccv..."
+                autoComplete='off'
+                value={ccv}
+                onChange={e => setCcv(Number(e.target.value))}
+              />
+            </div>
+            <button
+              className="add-card-submit-button"
+              onClick={handleAddCard}
+            >Cadastrar Cartão</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
