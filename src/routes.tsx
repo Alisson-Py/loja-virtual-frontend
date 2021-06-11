0db151fa71c038aef552ea5dc5c1324b5203f14c
Alisson-Py <alissilva1231@hotmail.com>
2021-06-11 08:35:47 -0300

    feat: add buy product route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import AddCart from './pages/AddCard';
+import BuyProduct from './pages/BuyProduct';
 import Login from './pages/Login';
 
 import Product from './pages/Product';
@@ -19,6 +20,7 @@ export default function Routes() {
       <Route exact path="/product/:id" component={ProductsDetails} />
       <Route exact path="/products/create" component={ProductCreate} />
       <Route exact path="/add-card" component={AddCart} />
+      <Route exact path="/product/buy/:id" component={BuyProduct} />
       {/* <Route exact path="/product/update/:id" component={} /> */}
       
       <Route exact path="/profile" component={Profile} />
