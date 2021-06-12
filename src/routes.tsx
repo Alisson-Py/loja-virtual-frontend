6e217d559a651f7ef129c700b95f9b70e8b3aa02
Alisson-Py <alissilva1231@hotmail.com>
2021-06-12 14:06:50 -0300

    feat: add Show transactions route path

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,6 +9,7 @@ import ProductCreate from './pages/ProductCreate';
 import ProductsDetails from './pages/ProductsDetails';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
+import ShowPayment from './pages/ShowTransactions';
 import UpdateProfile from './pages/UpdateProfile';
 
 
@@ -21,7 +22,7 @@ export default function Routes() {
       <Route exact path="/products/create" component={ProductCreate} />
       <Route exact path="/add-card" component={AddCart} />
       <Route exact path="/product/buy/:id" component={BuyProduct} />
-      {/* <Route exact path="/product/update/:id" component={} /> */}
+      <Route exact path="/checkout" component={ShowPayment} />
       
       <Route exact path="/profile" component={Profile} />
       <Route exact path="/profile/update" component={UpdateProfile} />
